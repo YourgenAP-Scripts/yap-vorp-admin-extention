7283c892cd13b3ca8dc046381e45ce45e0953d1a
YourgenAP <79701428+YourgenAP@users.noreply.github.com>
2022-09-12 02:50:34 +0200

    Update admin_extension.sql

--- admin_extension.sql
+++ admin_extension.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `permissions`  (
   `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   UNIQUE INDEX `identifier`(`identifier`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
